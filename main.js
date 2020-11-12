8646dc3af9bd9e40990d02db7ae13f7200c26f05
Donnell Maylor <donnellthornill@gmail.com>
2020-11-11 19:28:32 -0500

    completed variables

---
+++ main.js
@@ -0,0 +1,13 @@
+//alert('Outputing information');
+console.log('Outputting information');
+var money = 1000;
+var name = 'AJ will';//sting literal 
+var isAgeValid = true;//boolean
+var empty = null;//Clears 
+var moneyAmount;
+
+console.log(money);
+console.log(name);
+console.log(isAgeValid);
+console.log(empty);
+console.log(moneyAmount);
